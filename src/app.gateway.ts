3239b370de1e86196646071792d435f7b3580010
Van Hiep <levanhiep.95.95@gmail.com>
2021-05-10 20:12:09 +0700

    init socket

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,9 +1,35 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
-
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  WsResponse,
+} from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import { Socket, Server } from 'socket.io';
 @WebSocketGateway()
-export class AppGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+export class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  private logger: Logger = new Logger('AppGateway');
+
+  afterInit(server: Server) {
+    this.logger.log('Initialized!');
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client connected: ${client.id}`);
+  }
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
+  @SubscribeMessage('msgToServer')
+  handleMessage(client: Socket, text: string): WsResponse<string> {
+    // client.emit('msgToClient', text);
+    return {
+      event: 'msgToClient',
+      data: text,
+    };
   }
 }
